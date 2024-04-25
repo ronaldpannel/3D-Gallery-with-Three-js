f66fb60f3545202d07aba7b0255b63c99c03a7f8
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2024-04-26 00:26:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
 camera.position.z = 5;
 
 renderer = new THREE.WebGLRenderer();
@@ -22,14 +21,57 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 const cubeGeo = new THREE.BoxGeometry(1, 1, 1);
-const cubeMat = new THREE.MeshBasicMaterial({ color: "green" });
+const cubeMat = new THREE.MeshStandardMaterial({ color: "green" });
 const cube = new THREE.Mesh(cubeGeo, cubeMat);
 scene.add(cube);
 
+//floor
+const texture = new THREE.TextureLoader().load("WoodFloor.png");
+const floorMAt = new THREE.MeshBasicMaterial({map: texture})
+const floorGeo = new THREE.PlaneGeometry(50, 50);
+const floor = new THREE.Mesh(floorGeo, floorMAt);
+floor.rotation.x = Math.PI /4
+floor.position.y = Math.PI
+
+scene.add(floor);
+
+const hemLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+scene.add(hemLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
+
+
+
 function animate() {
   cube.rotation.y += 0.005;
-  cube.rotation.z += 0.005;
+  cube.rotation.x += 0.005;
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
 animate();
+//key controls
+document.addEventListener('keydown', onKeydown, false)
+
+function onKeydown(e){
+console.log(e.key)
+if (e.key === "ArrowLeft" || e.key.toLowerCase() === 'a') {
+  camera.translateX(0.1);
+} else if (e.key === "ArrowRight" || e.key.toLowerCase() === "d") {
+  camera.translateX(-0.1);
+} else if (e.key === "ArrowUp" || e.key.toLowerCase() === "w") {
+  camera.translateY(-0.1);
+} else if (e.key === "ArrowDown" || e.key.toLowerCase() === "s") {
+  camera.translateY(0.1);
+}
+}
+
+
+
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth/window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
